9618bc698a9cfc97aa977b00bb24a6b0eaa52ee3
Jinho <breezjin@gmail.com>
2021-06-29 18:11:38 +0900

    finished game!!!

--- js/baseball-index.js
+++ js/baseball-index.js
@@ -4,70 +4,85 @@ console.log("hello, vanilla.");
 // 2. 게임 시작 버튼을 클릭 했을때, 랜덤한 세 자리 숫자 만들기 (사용자에게 보여주진 않습니다.)
 
 const btnStart = document.querySelector('#btn-start');
+const source = [0,1,2,3,4,5,6,7,8,9]
 let answer = [];
-let userTry01 = [];
-let userTry02 = [];
-let userTry03 = [];
-let userTry04 = [];
-let userTry05 = [];
-let userTry06 = [];
-let userTry07 = [];
-let userTry08 = [];
-let userTry09 = [];
-let userTry10 = [];
-let userTryList = [userTry01, userTry02, userTry03, userTry04, userTry05, userTry06, userTry07, userTry08, userTry09, userTry10];
-let beforeTryNumer = 0;
+let userTry = [];
+let userTryList = [];
 
-function makeRandomNumber () {
-    let randomNumber = Math.floor(Math.random() * 1000);
-    console.log('랜덤 숫자 생성 : ' + randomNumber); //검증용
-    let str = randomNumber.toString();
+function makeAnswer () {
     for (let i = 0; i < 3; i++) {
-        let digit = str.charAt(i);
-        answer.push(Number(digit));
+        let digit = source[Math.floor(Math.random() * 10)];
+        answer.push(digit);
     }
-    console.log('생성된 숫자를 answer(arr)로 : ' + answer); //검증용
+    console.log('랜덤 숫자 생성 : ' + answer); //검증용
+
+    start.classList.add('hidden');
+    game.classList.remove('hidden');
 };
 
-btnStart.addEventListener('click', makeRandomNumber);
+btnStart.addEventListener('click', makeAnswer);
 
 // 3. 숫자 입력칸 만들기 [done]
-// 4. 사용자가 엔터키를 클릭 했을때, 입력값이 세자리 숫자가 아닌 경우 경고창 띄워주기
-
 const input = document.querySelector('#input-numbers');
 const btnCheck = document.querySelector('#btn-enter');
-const validation = document.querySelector('#validation');
+const start = document.querySelector('#start');
+const game = document.querySelector('#game');
+const end = document.querySelector('#end');
+const tryNumberArea = document.querySelector('#try-number-area');
+const tryNumber = document.querySelector('#try-number');
 
 const result = document.querySelectorAll('.result');
 const userTryArea = document.querySelectorAll('.n');
+const strikeDot = document.querySelectorAll('.strike-dot');
+const ballDot = document.querySelectorAll('.ball-dot');
 
-const tryNumberArea = document.querySelector('#try-number');
-const strikeDot = document.querySelector('#strike-dot');
-const ballDot = document.querySelector('#ball-dot');
+let checkAnswerCalled = 0;
 
 function checkAnswer () {
+    start.classList.add('hidden');
+
     let inputNumbers = input.value;
-    userTry.splice(0, userTry.length);
     console.log('사용자가 입력한 값 : ' + inputNumbers); //검증용
 
+    // 4. 사용자가 엔터키를 클릭 했을때, 입력값이 세자리 숫자가 아닌 경우 경고창 띄워주기
     if (inputNumbers.length !== 3) {
         alert('3자리 숫자를 입력해주세요.');
         return;
     }
 
-    for (let i = 0; i < inputNumbers.length; i++) {
-        let digit2 = inputNumbers.charAt(i);
-        userTry.push(Number(digit2));
-        userTryArea[i].innerHTML = userTry[i];
+    // 9. 사용자가 10회까지 시도할 수 있도록 제한합니다.
+    checkAnswerCalled++;
+    tryNumberArea.classList.remove('hidden');
+    tryNumber.innerHTML = checkAnswerCalled;
+    if (checkAnswerCalled > 10) {
+        showFail();
     }
-    console.log('입력한 숫자를 userTry(arr)로 : ' + userTry); //검증용
+    result[10 - checkAnswerCalled].classList.remove('hidden');
 
-    beforeTryNumer = beforeTryNumer + 1;
-    tryNumberArea.innerHTML = beforeTryNumer;
+    for (let i = 2; i >= 0; i--) {
+        let digit2 = inputNumbers.charAt(2 - i);
+        userTry.push(Number(digit2));
+        userTryArea[30 - (checkAnswerCalled * 3) + (2 - i)].innerHTML = userTry[2 - i];
+        // 5. 사용자가 엔터키를 클릭 했을때, 2단계에서 생성한 숫자와 사용자의 입력값 비교하기
+        // 6. 각 자리 별로 비교하고, 같은 자리에 같은 숫자가 몇개 있는지 판별합니다. (스트라이크 갯수)
+        // 7. 각 자리 별로 비교하고, 다른 자리에 같은 숫자가 몇개 있는지 판별합니다. (볼 갯수)
+        // 8. 화면에 스트라이크와 볼의 갯수를 표기합니다.
+        // *** 최근에 입력한 값이 바로바로 보이도록 구성
+        if (userTry[2 - i] === answer[2 - i]) {
+            strikeDot[30 - (checkAnswerCalled * 3) + (2 - i)].classList.remove('hidden')
+        } else if (answer.includes(userTry[2 - i])) {
+            ballDot[30 - (checkAnswerCalled * 3) + (2 - i)].classList.remove('hidden')
+        }
+    }
 
-    userTryList.push(userTry);
-    console.log('userTryList(arr) 결과값 : '); //검증용
-    console.log(userTryList); //검증용
+    if (answer[0] === userTry[0] && answer[1] === userTry[1] && answer[2] === userTry[2]) {
+        showSuccess();
+    }
+
+    userTry.splice(0, userTry.length);
+    input.value = '';
+    console.log('현재 답은 : ' + answer); //검증용
+
 };
 
 btnCheck.addEventListener('click', checkAnswer);
@@ -78,12 +93,30 @@ input.addEventListener('keyup', function (e) {
     }
 });
 
+const success = document.querySelector('#success');
+const fail = document.querySelector('#fail');
+const answerResult = document.querySelector('#answer');
+const restart = document.querySelector('#btn-restart');
 
-// 5. 사용자가 엔터키를 클릭 했을때, 2단계에서 생성한 숫자와 사용자의 입력값 비교하기
-// 6. 각 자리 별로 비교하고, 같은 자리에 같은 숫자가 몇개 있는지 판별합니다. (스트라이크 갯수)
-// 7. 각 자리 별로 비교하고, 다른 자리에 같은 숫자가 몇개 있는지 판별합니다. (볼 갯수)
-// 8. 화면에 스트라이크와 볼의 갯수를 표기합니다.
-// 9. 사용자가 10회까지 시도할 수 있도록 제한합니다.
+function showFail() {
+    game.classList.add('hidden');
+    end.classList.remove('hidden');
+    fail.classList.remove('hidden');
+};
+
+function showSuccess() {
+    game.classList.add('hidden');
+    end.classList.remove('hidden');
+    success.classList.remove('hidden');
+    answerResult.innerHTML = answer;
+};
+
+function restartGame() {
+    location.reload();
+};
+
+restart.addEventListener('click', restartGame);
+
 // 10. 게임 재시작 버튼을 만들고, 재시작 할 수 있도록 합니다.
 
 
